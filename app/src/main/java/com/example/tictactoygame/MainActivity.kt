1a5377c34e1f5a640b77215cd0f3941f738a7a41
AzeezFazry <azeezfazry@gmail.com>
2021-04-06 19:19:27 +0530

    Tic Tac Game

---
+++ app/src/main/java/com/example/tictactoygame/MainActivity.kt
@@ -0,0 +1,34 @@
+package com.example.tictactoygame
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.Toast
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+
+    fun buClick(view:View){
+
+        var buSelect:Button = view as Button
+        var cellId = 0
+        when(buSelect.id){
+            R.id.bu1 -> cellId = 1
+            R.id.bu2 -> cellId = 2
+            R.id.bu3 -> cellId = 3
+            R.id.bu4 -> cellId = 4
+            R.id.bu5 -> cellId = 5
+            R.id.bu6 -> cellId = 6
+            R.id.bu7 -> cellId = 7
+            R.id.bu8 -> cellId = 8
+            R.id.bu9 -> cellId = 9
+        }
+        Toast.makeText(this, "ID: "+cellId, Toast.LENGTH_LONG).show()
+
+    }
+
+}
